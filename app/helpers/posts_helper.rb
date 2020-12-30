90ee05d9c66fbd5e0888558cc8f858b58dd74f2a
bswan0002 <bswan0002@gmail.com>
2020-12-30 11:20:49 -0800

    added validations

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,3 @@
 module PostsHelper
-  def admin?
-    current_user && current_user.admin
-  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,6 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :users, through: :likes
   has_many :users, through: :comments
+
+  validates_presence_of :title, :body
 end
