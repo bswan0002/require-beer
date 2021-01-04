8bca16cac805c582830480474491f3641e0e7649
choim3 <choim3@spu.edu>
2021-01-04 11:57:17 -0800

    comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,62 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
+
+
+  # GET /comments/new
+  def new
+    @comment = Comment.new
+  end
+
+  # GET /comments/1/edit
+  def edit
+  end
+
+  # comment /comments
+  # comment /comments.json
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.user = current_user
+    @comment.save
+    redirect_to post_path(@post)
+    # respond_to do |format|
+    #   if @comment.save
+    #     format.html { notice: 'comment was successfully created.' }
+    #     format.json { status: :created, location: @comment }
+    #   else
+    #     format.json { render json: @comment.errors, status: :unprocessable_entity }
+    #   end
+    # end
+  end
+
+  # PATCH/PUT /comments/1
+  # PATCH/PUT /comments/1.json
+  def update
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to @comment, notice: 'comment was successfully updated.' }
+        format.json { render :show, status: :ok, location: @comment }
+      else
+        format.html { render :edit }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /comments/1
+  # DELETE /comments/1.json
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to comments_url, notice: 'comment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+
+  private
+    # Only allow a list of trusted parameters through.
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
+end
