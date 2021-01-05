c0c3b70ef298fa0bbc23bd077776681403f7949b
bswan0002 <bswan0002@gmail.com>
2021-01-05 10:38:11 -0800

    need to pull

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   end 
 
   put '/post/:id/like', to: 'posts#like', as: 'like'
+  get '/posts/sort_by', to: 'posts#sort_by', as: 'sort_by'
 
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
